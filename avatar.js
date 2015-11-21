4fea1ac5d6992b91869c63bbbb7c904856c0b780
unknown <michalnebbia@gmail.com>
2015-11-21 17:41:18 +0100

    pridane nove ikonky a pridane okno pre admina

--- avatar.js
+++ avatar.js
@@ -1,15 +1,15 @@
-var okno;
+var avatarSkladanie;
 
 (function () {
 
-  okno = function () {
-    RWindow.call (this, 100, 90, 650, 353, 'owl24.png');
+  avatarSkladanie = function () {
+    RWindow.call (this, 100, 90, 650, 353, 'avatar-skladanie-min.png');
     var self = this;
 
     this.change_cfg ({bgcolor:'rgb(164, 234, 164)', selcolor:'rgb(81, 218, 129)'});
     this.resizeable = false;
     this.dragable = true;
-    this.lab.innerHTML = 'Pokus';
+    this.lab.innerHTML = 'Vyskladaj si avatara';
     this.lab.style.textAlign = 'center';
     this.lab.style.marginLeft = '0px';
 
@@ -26,7 +26,7 @@ var okno;
     });
 
 
-    this.ico = Asset.image ('obrazky/owl50.png');
+    this.ico = Asset.image ('obrazky/avatar-skladanie.png');
     menu.Add (this.ico);
     this.ico.addEventListener ('mousedown', function (e) {
       self.show ();
@@ -42,13 +42,13 @@ var okno;
   (function (){
     function Tmp () {};
     Tmp.prototype = RWindow.prototype;
-    okno.prototype = new Tmp ();
+    avatarSkladanie.prototype = new Tmp ();
   })();
 
 })();
 
 ///////////////////////////////////////////////////
-okno.prototype.moja = function () {
+avatarSkladanie.prototype.moja = function () {
   var canvas = new fabric.Canvas('myCanvas');
 
   // create a rectangle object
@@ -68,6 +68,75 @@ okno.prototype.moja = function () {
 }
 ///////////////////////////////////////////////////
 
+var avatarAdmin;
+
+(function () {
+
+  avatarAdmin = function () {
+    RWindow.call (this, 100, 90, 650, 353, 'avatar-skladanie-min.png');
+    var self = this;
+
+    this.change_cfg ({bgcolor:'rgb(164, 234, 164)', selcolor:'rgb(81, 218, 129)'});
+    this.resizeable = false;
+    this.dragable = true;
+    this.lab.innerHTML = 'Vyskladaj si avatara';
+    this.lab.style.textAlign = 'center';
+    this.lab.style.marginLeft = '0px';
+
+    ////////////////////////////////////////////////////
+    self.con.innerHTML = '<canvas id="myCanvas" width="400" height="400" style="border:1px solid #c3c3c3;"></canvas>';
+    ////////////////////////////////////////////////////
+    
+
+    this.Bclose.style.visibility = 'visible';
+    this.Bclose.addEventListener ('mousedown', function (e) {
+      self.hide ();
+      menu.Add (self.ico);
+      e.stopPropagation ();
+    });
+
+
+    this.ico = Asset.image ('obrazky/avatar-admin.png');
+    menu.Add (this.ico);
+    this.ico.addEventListener ('mousedown', function (e) {
+      self.show ();
+      menu.Rem (self.ico);
+      e.stopPropagation ();
+      
+      // volame moju funkciu ktora z canvasu spravi fabric.js canvas
+      self.moja();
+    });
+
+  };
+
+  (function (){
+    function Tmp () {};
+    Tmp.prototype = RWindow.prototype;
+    avatarAdmin.prototype = new Tmp ();
+  })();
+
+})();
+
+///////////////////////////////////////////////////
+avatarAdmin.prototype.moja = function () {
+  var canvas = new fabric.Canvas('myCanvas');
+
+  // create a rectangle object
+  var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20
+  });
+  //ukladanie objektov
+  var json = JSON.stringify( canvas.toJSON() );
+  console.log(json);
+  
+  // "add" rectangle onto canvas
+  canvas.add(rect);
+}
+///////////////////////////////////////////////////
 
 
 var slimak;
